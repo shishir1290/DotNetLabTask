593b3de39f3f090e5d1a66c1b7db15ede1912295
unknown <shishir1290@gmail.com>
2023-10-07 18:35:22 +0600

    Lab Taskk 2

---
+++ Lab2/Lab2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(IndexModel signup)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("About");
+            }
+            return View(signup);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lab2/Lab2/Models/DateOfBirthAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class DateOfBirthAttribute : ValidationAttribute
+{
+    private readonly int _minimumAge;
+
+    public DateOfBirthAttribute(int minimumAge)
+    {
+        _minimumAge = minimumAge;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            if (age < _minimumAge)
+            {
+                return new ValidationResult($"You must be at least {_minimumAge} years old.");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
